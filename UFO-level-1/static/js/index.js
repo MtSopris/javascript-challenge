f23e46d01816d64ae44d1102658a9bd533c9df00
MtSopris <73788171+MtSopris@users.noreply.github.com>
2021-03-25 22:24:05 -0600

    final functioning code that returns the input or keep the original data

--- UFO-level-1/static/js/index.js
+++ UFO-level-1/static/js/index.js
@@ -2,39 +2,46 @@ var tbody = d3.select('tbody')
 
 console.log(data)
 
-// data.forEach((ufo) => {
-//     var row = tbody.append("tr");
-//     Object.entries(ufo).forEach(([key, value]) => {
-//         var cell = row.append("td");
-//         cell.text(value);
-//     });
-// });
-
+// //assign data source to new variable dataset
 var fillData = data;
-var inputFeild = d3.select('#datetime');
-var button = d3.select('#filter-btn');
 
-// function handleClick() {
-//     // console.log('clicked');
-//     console.log(d3.event.target);
-// }
-
-button.on('click');
-inputFeild.on('change', function () {
-    var newText = d3.event.target.value;
-    console.log(newText);
-    var filteredData = fillData.filter(date => date.datetime === newText);
-
-    console.log(filteredData);
-
-    filteredData.forEach(function (FilterDate) {
-        console.log(FilterDate);
-        var row = tbody.append("tr");
-
-        Object.entries(FilterDate).forEach(function ([key, value]) {
-            console.log(key, value);
+// build table from data set
+function BuildTable(data){
+    // clear any existing data in html table
+    tbody.html("");
+    // loop throug each row in data and populate row/element into table with keys as columns
+    data.forEach(function (element) {
+        var row = tbody.append("tr");//tr means table row
+        // for each dict/element assign the value to a cell in the table
+        Object.values(element).forEach((value) => {
             var cell = row.append('td');
             cell.text(value);
         });
     });
-});
+}; 
+
+// construct the table using the BuildTable function from the variable fillData
+BuildTable(fillData)
+
+// filter dataset by date entered in input feild
+function dateData(){
+    //assign data to new variable
+    var filterData = data;
+    //grab the value of the text inputed to the input box
+    var inputFeild = d3.select('#datetime').property('value');
+    console.log(inputFeild)
+    //use if to varify that there was input entered if not it retuns false and keeps original dats displayed
+    if (inputFeild){
+        //collect the rows that contain input feild date
+        var filterData=filterData.filter(row => row['datetime'] === inputFeild);
+    }
+    console.log(filterData)
+    //construct a new table of the filtered data
+    BuildTable(filterData)
+}
+//when the filter button is clicked run the dateData function and place it in the table in the html page.
+d3.select('#filter-btn').on('click', dateData);
+
+    
+
+    
